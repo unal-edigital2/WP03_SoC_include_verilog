a3dedfab6496961b968c002b13f3ce95817d075d
ferney <fabeltranm@unal.edu.com>
2020-12-03 07:14:52 -0500

    initial

---
+++ SoC_project_includeVerilog/firmware/camara.d
@@ -0,0 +1,36 @@
+camara.o: camara.c \
+ /opt/litex/litex/litex/soc/software/include/base/stdio.h \
+ /opt/litex/litex/litex/soc/software/include/base/stddef.h \
+ /opt/litex/litex/litex/soc/software/include/base/stdlib.h \
+ /opt/litex/litex/litex/soc/software/include/base/string.h camara.h \
+ /opt/litex/litex/litex/soc/software/include/base/uart.h \
+ /opt/litex/litex/litex/soc/cores/cpu/picorv32/irq.h \
+ /opt/litex/litex/litex/soc/cores/cpu/picorv32/system.h \
+ /home/ferney/Documentos/wp06_SoC_includeVerilog/SoC_project_includeVerilog/build/nexys4ddr/software/include/generated/csr.h \
+ /home/ferney/Documentos/wp06_SoC_includeVerilog/SoC_project_includeVerilog/build/nexys4ddr/software/include/generated/soc.h \
+ /opt/litex/litex/litex/soc/software/include/base/stdint.h \
+ /opt/litex/litex/litex/soc/software/include/hw/common.h
+
+/opt/litex/litex/litex/soc/software/include/base/stdio.h:
+
+/opt/litex/litex/litex/soc/software/include/base/stddef.h:
+
+/opt/litex/litex/litex/soc/software/include/base/stdlib.h:
+
+/opt/litex/litex/litex/soc/software/include/base/string.h:
+
+camara.h:
+
+/opt/litex/litex/litex/soc/software/include/base/uart.h:
+
+/opt/litex/litex/litex/soc/cores/cpu/picorv32/irq.h:
+
+/opt/litex/litex/litex/soc/cores/cpu/picorv32/system.h:
+
+/home/ferney/Documentos/wp06_SoC_includeVerilog/SoC_project_includeVerilog/build/nexys4ddr/software/include/generated/csr.h:
+
+/home/ferney/Documentos/wp06_SoC_includeVerilog/SoC_project_includeVerilog/build/nexys4ddr/software/include/generated/soc.h:
+
+/opt/litex/litex/litex/soc/software/include/base/stdint.h:
+
+/opt/litex/litex/litex/soc/software/include/hw/common.h:
